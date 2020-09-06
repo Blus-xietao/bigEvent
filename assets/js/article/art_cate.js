fd66d17f937cf2c768dd9c69c45a813de86f57dd
xietao <425086294@qq.com>
2020-09-06 17:05:24 +0800

    文章分类完成

---
+++ assets/js/article/art_cate.js
@@ -0,0 +1,101 @@
+$(function () {
+    var layer = layui.layer
+    var form = layui.form
+      initArtCateList()
+    // 获取文章列表函数
+    function   initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success(res) {
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+    // 为添加类别按钮绑定点击事件
+    var indexAdd = null
+    $('#btnaddart').on('click', function () {
+        indexAdd = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章分类',
+            content: $('#alateAdd').html()
+        });
+    })
+    // 通过代理的形式，为 form-add 表单绑定 submit 事件
+    $('body').on('submit', "#form-add", function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: "post",
+            url: "/my/article/addcates",
+            data: $(this).serialize(),
+            success(res) {
+                if (res.status !== 0) {
+                    return layer.msg('新增分类失败！')
+                }
+                  initArtCateList()
+                layer.msg('新增分类成功！')
+                layer.close(indexAdd)
+            }
+        })
+    })
+    // 通过代理的形式，为 btn-edit 按钮绑定点击事件
+    var indexEdit = null
+    $('tbody').on('click', '.btn-edit', function () {
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '修改文章分类',
+            content: $('#alateEdit').html()
+        });
+        // 获取被点击按钮的data-id属性值
+        var id = $(this).attr('data-id')
+        // 发起请求获取对应分类的数据
+        $.ajax({
+            method: "get",
+            url: '/my/article/cates/' + id,
+            success(res) {
+                form.val('form-edit', res.data)
+            }
+        })
+    })
+    // 通过代理的形式，为修改分类的表单绑定 submit 事件
+    $('body').on('submit', '#form-edit', function(e) {
+        e.preventDefault()
+        $.ajax({
+          method: 'POST',
+          url: '/my/article/updatecate',
+          data: $(this).serialize(),
+          success: function(res) {
+            console.log(res);
+            if (res.status !== 0) {
+              return layer.msg('更新分类数据失败！')
+            }
+            layer.msg('更新分类数据成功！')
+            layer.close(indexEdit)
+            initArtCateList()
+          }
+        })
+      })
+
+    // 通过代理的形式，为删除按钮绑定点击事件
+    $('tbody').on('click', '.btn-delete', function () {
+        var id = $(this).attr('data-id')
+        // 提示用户是否要删除
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/deletecate/' + id,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除分类失败！')
+                    }
+                    layer.msg('删除分类成功！')
+                    layer.close(index)
+                      initArtCateList()
+                }
+            })
+        })
+    })
+})
